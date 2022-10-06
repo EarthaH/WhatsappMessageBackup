49c7ae803190f66e5e21bd62ebcf4c51155a86e4
Eartha Naylor <earthanaylor@gmail.com>
2022-10-06 13:28:09 +0200

    WIP: mongodb dao

--- pkg/dao/mongodb.go
+++ pkg/dao/mongodb.go
@@ -1,1 +1,23 @@
 package dao
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"hogsback.com/whatsapp/pkg/logger"
+)
+
+func Init() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://cluster0.yhlijvd.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"))
+
+	if err != nil {
+		logger.Error(err)
+	}
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		logger.Error(err)
+	}
+}
